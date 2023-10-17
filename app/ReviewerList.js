8a43cd4a08a54f4f9494ce20877626b839f2d9f4
Mario Salgado <mapsalgado92@gmail.com>
2023-10-17 14:58:57 +0100

    FIXED SELECTORS

--- app/ReviewerList.js
+++ app/ReviewerList.js
@@ -1,6 +1,8 @@
 import { uniqueId } from "lodash"
+import { useState } from "react"
 
 const ReviewerList = ({ label, filter_handler, field, data }) => {
+  const [selected, setSelected] = useState("All Reviewers")
   return (
     <>
       <div className="field">
@@ -8,13 +10,17 @@ const ReviewerList = ({ label, filter_handler, field, data }) => {
         <div className="control select is-small is-fullwidth">
           <select
             onChange={(e) => {
+              setSelected(e.target.value)
               filter_handler(field, e.target.value)
             }}
+            value={selected}
           >
             {["All Reviewers", ...new Set(data.map((record) => record[field]))]
               .filter((r) => r)
               .map((r) => (
-                <option key={uniqueId()}>{r}</option>
+                <option key={uniqueId()} value={r}>
+                  {r}
+                </option>
               ))}
           </select>
         </div>
